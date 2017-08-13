439a5a9e223b3ca8c667c7c97b120bf206938e82
James Pharaoh <james@pharaoh.uk>
2017-08-13 09:15:57 +0000

    work on object verification, use hyphenated names in record specs, move event types to own file, other general refactoring

--- fixture/WalletFixtureProvider.java
+++ fixture/WalletFixtureProvider.java
@@ -9,7 +9,7 @@ import wbs.framework.component.config.WbsConfig;
 import wbs.framework.database.Database;
 import wbs.framework.database.OwnedTransaction;
 import wbs.framework.entity.record.GlobalId;
-import wbs.framework.fixtures.FixtureProvider;
+import wbs.framework.fixtures.Fixture;
 import wbs.framework.logging.LogContext;
 import wbs.framework.logging.TaskLogger;
 
@@ -26,7 +26,7 @@ import wbs.services.wallet.model.WalletServiceRec;
 @PrototypeComponent ("walletFixtureProvider")
 public
 class WalletFixtureProvider
-	implements FixtureProvider {
+	implements Fixture {
 
 	// singleton dependencies
 
